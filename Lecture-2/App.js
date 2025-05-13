e3b1bfac2b186e54fdefc883206821bb457f92a5
deepak <2001dkmaurya@gmail.com>
2025-05-14 02:24:44 +0530

    lecture-2 in progress

---
+++ Lecture-2/App.js
@@ -0,0 +1,11 @@
+         import React from "react"; //importing react from node module
+         import ReactDOM from "react-dom/client"; //importing react-dom from node module
+        //job of core react
+        const heading=React.createElement("h1",{id:"heading"},"Welcome! to Food Mania"); //heading is an object
+
+        //job of react dom
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+
+
+        root.render(heading);//render mathod convert the heading object into heading tag and put into the dom
+        //note:what is the {} object inside the createElement it is the place where we give attributes to our tag that is let give h1 a id with name heading 
